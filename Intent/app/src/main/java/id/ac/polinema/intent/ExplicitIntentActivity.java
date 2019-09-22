e129eda940855c804e54b63186c2a626b445661d
Septa Kusumaningtyas <septakusumaningtyas@gmail.com>
2019-09-22 17:10:26 +0700

    Explicit Intent, Implicit Intent, Android Bundle

--- Intent/app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
+++ Intent/app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
@@ -4,15 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class ExplicitIntentActivity extends AppCompatActivity {
+    private EditText inputName;
+    private TextView outputText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_explicit_intent);
+        inputName = findViewById(R.id.input_name);
+        outputText = findViewById(R.id.text_output);
     }
 
+    public void handleSubmit(View view) {
+        String name = inputName.getText().toString();
+        outputText.setText("Hello " + name + " Congratulations!");
+    }
 }
--- Intent/app/src/main/java/id/ac/polinema/intent/ProfileBundleActivity.java
+++ Intent/app/src/main/java/id/ac/polinema/intent/ProfileBundleActivity.java
@@ -3,19 +3,31 @@ package id.ac.polinema.intent;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class ProfileBundleActivity extends AppCompatActivity {
+    private TextView usernameText;
+    private TextView nameText;
+    private TextView ageText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_bundle);
 
-        // TODO: bind here
+        usernameText = findViewById(R.id.text_username);
+        nameText = findViewById(R.id.text_name);
+        ageText = findViewById(R.id.text_age);
 
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
-            // TODO: display value here
+            String username = extras.getString(String.valueOf(usernameText));
+            usernameText.setText(username);
+            String name = extras.getString(String.valueOf(nameText));
+            nameText.setText(name);
+            String age = extras.getString(String.valueOf(ageText));
+            ageText.setText(age);
         }
     }
 }
